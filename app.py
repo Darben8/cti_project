26957347fbbc90eb474ecabde6ba2fe0e2f8116e
otis27-maker <otis.service27@gmail.com>
2026-04-23 04:21:28 +0000

    Made some adjustments to the Milestone 3 section in app.py, and updated the preliminary visualizations headers in the dashboard page.

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ CHASE_BLUE = "#117ACA"
 DARK_BLUE = "#0B3A69"
 LIGHT_BG = "#F4F8FC"
 
-st.markdown(
-    f"""
-    <style>
-    .stApp {{ background-color: {LIGHT_BG}; }}
-    h1, h2, h3 {{ color: {DARK_BLUE}; }}
-    [data-testid="stMetricValue"] {{ color: {CHASE_BLUE}; }}
-    .block-container {{ padding-top: 1.2rem; }}
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# st.markdown(
+#     f"""
+#     <style>
+#     .stApp {{ background-color: {LIGHT_BG}; }}
+#     h1, h2, h3 {{ color: {DARK_BLUE}; }}
+#     [data-testid="stMetricValue"] {{ color: {CHASE_BLUE}; }}
+#     .block-container {{ padding-top: 1.2rem; }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True,
+# )
 
 st.title("🏦 Cyber Threat Intelligence Platform: U.S. Banking")
 #st.caption("Milestone 1 - Industry baseline, threat trends, diamond models, and dashboard starter.")
@@ -50,12 +50,18 @@ with st.container(border=True):
         st.checkbox("We download threat intelligence data from phishtank and  in the data folder", value=True, disabled=True)
 
 with st.container(border=True):
-    with st.expander("📌 Milestone 3"):
-        st.header("✅ Milestone 3: New Features")
-        st.checkbox("Analytical Approaches: Phishing URL Text Mining & Ransomware Correlation", value=True, disabled=True)
-        st.checkbox("Interactive Analytics Panel with parameter toggles", value=True, disabled=True)
-        st.checkbox("Preliminary Visualizations (Temporal Trends & Malware Distribution)", value=True, disabled=True)
-        st.checkbox("Operational Metrics (MTTD/MTTR explanation)", value=True, disabled=True)
+     with st.expander("📌 Milestone 3"):
+        st.subheader("✅ Milestone 3: New Features")
+        st.checkbox("Analytical Approaches: Phishing URL Text Mining & Ransomware Event Correlation", value=True, disabled=True)
+        st.checkbox("Interactive Analytics Panel: Multi-source filtering with unique-key state management", value=True, disabled=True)
+        st.checkbox("Preliminary Visualizations: Temporal trends and threat distribution with process/value justifications", value=True, disabled=True)
+        st.checkbox("Operational Metrics: Implementation of MTTD reduction estimates and Indicator Precision tracking", value=True, disabled=True)
+        st.checkbox("Enhanced UI: Wide-mode dashboard layout and automated EST refresh timestamps", value=True, disabled=True)
+        st.checkbox("Validation: Added error analysis and data limitation documentation", value=True, disabled=True)
+
+#st.success("Milestone 3 requirements fully implemented and documented in the Dashboard.")
+
+st.divider()
 
 col1, col2 = st.columns([1.2, 1], gap="large")
 
