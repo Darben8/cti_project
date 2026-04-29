56ee28914765ad58c34aa0e18ae9814e06fc0769
Dar Ben <darkoabena473@gmail.com>
2026-04-29 19:35:02 -0400

    updates to app

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 pg = st.navigation(
     [
         st.Page("pages/0_Home.py",                                          title="Home",                           icon="🏠"),
-        st.Page("pages/1_Industry_Background.py",                           title="Industry Background",            icon="🏛️"),
+        st.Page("pages/1_Overview.py",                           title="Industry Overview",            icon="🏛️"),
         st.Page("pages/2_Threat_Trends.py",                                 title="Threat Trends",                  icon="📈"),
         st.Page("pages/3_Critical_Assets.py",                               title="Critical Assets",                icon="🔒"),
         st.Page("pages/4_Diamond_Models.py",                                title="Diamond Models",                 icon="♦️"),
@@ -25,7 +25,7 @@ pg = st.navigation(
         st.Page("pages/12_Future_Directions.py",                            title="Future Directions",              icon="🔭"),
         st.Page("pages/13_Key_insights_and_intelligence_summary.py",   title="Key Insights & Intelligence Summary", icon="🧩"),
         st.Page("pages/14_Operational_Intelligence_and_Dissemination.py",   title="Operational Intelligence & Dissemination", icon="🚨"),
-        st.Page("pages/15_Analytics.py",   title="Analytics", icon="📊"),
+        st.Page("pages/15_Analytics.py",   title="Role-based Analytics", icon="📊"),
     ]
 )
 
