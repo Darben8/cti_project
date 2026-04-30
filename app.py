fe29c184afca86f40c5cb7ff12e9b919d5c82fdf
Dar Ben <darkoabena473@gmail.com>
2026-04-30 12:19:51 -0400

    updates to app

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ pg = st.navigation(
         st.Page("pages/3_Data_Sources.py",                  title="Data Sources",                   icon="📂"),
         st.Page("pages/4_Key_insights.py",                  title="Key Insights & Intelligence Summary", icon="🧩"),
         st.Page("pages/5_Operational_Intelligence_and_Dissemination.py",   title="Operational Intelligence & Dissemination", icon="🚨"),
-        st.Page("pages/6_Analytics.py",                     title="Role-based Analytics",      icon="📊"),
+        st.Page("pages/6_Analytics.py",                     title="Analytical Approaches",      icon="📊"),
         st.Page("pages/7_Future_Directions.py",             title="Future Directions",         icon="🔭"),
         st.Page("pages/8_Actionable_Outputs.py",   title="Actionable Outputs",              icon="🎯"),
         st.Page("pages/9_Team.py",                     title="Team",                        icon="👥"),
