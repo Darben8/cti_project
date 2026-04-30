54968d29bcba393eb9fd91bd889e392db87d0c82
Dar Ben <darkoabena473@gmail.com>
2026-04-30 01:19:51 -0400

    updated file names and app

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ st.set_page_config(
 
 pg = st.navigation(
     [
-        st.Page("pages/0_Home.py",                                          title="Home",                           icon="🏠"),
-        st.Page("pages/1_Overview.py",                                      title="Overview",                        icon="🏛️"),
-        st.Page("pages/2_Dashboard.py",                                     title="Dashboard",                      icon="📊"),
-        st.Page("pages/3_Data_Sources.py",                                  title="Data Sources",                   icon="📂"),
-        st.Page("pages/9_Analytical_approaches.py",                         title="Analytical Approaches",          icon="🧠"),
-        st.Page("pages/10_References.py",                                   title="References",                     icon="📚"),
-        st.Page("pages/11_Team.py",                                         title="Team",                           icon="👥"),
-        st.Page("pages/12_Future_Directions.py",                            title="Future Directions",              icon="🔭"),
-        st.Page("pages/13_Key_insights_and_intelligence_summary.py",   title="Key Insights & Intelligence Summary", icon="🧩"),
-        st.Page("pages/14_Operational_Intelligence_and_Dissemination.py",   title="Operational Intelligence & Dissemination", icon="🚨"),
-        st.Page("pages/15_Analytics.py",   title="Role-based Analytics", icon="📊"),
-        st.Page("pages/16_Actionable_Outputs.py",   title="Actionable Outputs")
+        st.Page("pages/0_Home.py",                          title="Home",                           icon="🏠"),
+        st.Page("pages/1_Overview.py",                      title="Overview",                        icon="🏛️"),
+        st.Page("pages/2_Dashboard.py",                     title="Dashboard",                      icon="📊"),
+        st.Page("pages/3_Data_Sources.py",                  title="Data Sources",                   icon="📂"),
+        st.Page("pages/4_Key_insights.py",                  title="Key Insights & Intelligence Summary", icon="🧩"),
+        st.Page("pages/5_Operational_Intelligence_and_Dissemination.py",   title="Operational Intelligence & Dissemination", icon="🚨"),
+        st.Page("pages/6_Analytics.py",                     title="Role-based Analytics",      icon="📊"),
+        st.Page("pages/7_Future_Directions.py",             title="Future Directions",         icon="🔭"),
+        st.Page("pages/8_Actionable_Outputs.py",   title="Actionable Outputs",              icon="🎯"),
+        st.Page("pages/9_Team.py",                     title="Team",                        icon="👥"),
+        st.Page("pages/10_References.py",              title="References",                  icon="📚"),
+    
     ]
 )
 
